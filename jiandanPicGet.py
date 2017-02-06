b1eb4b2b51e17bf264460250bdc63b40ba4da590
zhan <390961827@qq.com>
2017-02-06 22:44:54 +0800

    改为下载妹子图，完善爬虫

--- jiandanPicGet.py
+++ jiandanPicGet.py
@@ -10,9 +10,9 @@ import os
 # 下载图片
 # 翻页
 # 记录已下载图片
-initUrl = 'http://jandan.net/pic/page-'     # 初始页面
-saveDir = './pic'                           # 保存文件夹
-downPage = [2623, 2600]                     # 访问页码
+initUrl = 'http://jandan.net/ooxx/page-'     # 初始页面
+saveDir = './ooxx'                           # 保存文件夹
+downPage = {max: 2312, min: 2000}                    # 访问页码
 
 
 def get_pic_url(html):
@@ -23,25 +23,30 @@ def get_pic_url(html):
     for row in rows:
         url = row['href']
         picUrls.append(url)
-    print('urls is ', picUrls)
     return picUrls
 
 
 def down_pic_by_url(url):
     # 根据图片地址，下载图片到执行文件夹
-    r = requests.get('http:' + url)
+    dirName = nowPage                               # 储存文件夹名称
+    savePath = saveDir + '/' + dirName.__str__()    # 储存文件夹
     fileExtensionName = get_pic_url_file_extension_name(url)    # 文件扩展名
-    fileName = get_pic_url_file_name(url)   # 文件名
-    dirName = nowPage
-    savePath = saveDir + '/' + dirName.__str__()
+    fileName = get_pic_url_file_name(url)           # 文件名
+    fileSavePath = savePath + '/' + fileName
+    # 判断文件是否已经存在
+    if os.path.isfile(fileSavePath):
+        print('file %s already exist!' % fileSavePath)
+        return
+    r = requests.get('http:' + url)
     if not os.path.isdir(savePath):
         os.mkdir(savePath)
-    if (fileExtensionName == '.gif'):
-        # 文件为gif
-        print(url, ' is gif')
-    else:
-        i = Image.open(BytesIO(r.content))
-        i.save(savePath + '/' + fileName)        # 保存文件
+
+    f = open(savePath + '/' + fileName, 'wb')
+    f.write(r.content)
+    f.close()
+    # i = Image.open(BytesIO(r.content))
+    # i.save(savePath + '/' + fileName)        # 保存文件
+
 
 
 def get_pic_url_file_name(url:str):
@@ -58,12 +63,16 @@ def get_pic_url_file_extension_name(url:str):
 # 检测文件夹是否存在
 if not os.path.isdir(saveDir):
     os.mkdir(saveDir)
-
-for page in range(downPage[1], downPage[0]):
+print('Has %s page wait access.' % (downPage[max] - downPage[min]))
+for pageKey, page in enumerate(range(downPage[min], downPage[max])):
+    print('Access page is %s' % pageKey)
     nowPage = page                          # 当前使用页码
     mainUrl = initUrl + page.__str__()      # 爬取页面地址
-    print('url is ', mainUrl)
+    print('now page url is ', mainUrl)
     r = requests.get(mainUrl)
     picUrls = get_pic_url(r.content)        # 页面获取到的图片地址
-    for url in picUrls:
+    print('has %s url wait down' % picUrls.__len__())
+    for key, url in enumerate(picUrls):
+        print('%s pic is download...' % key)
         down_pic_by_url(url)
+        print('%s pic is down over!' % key)
